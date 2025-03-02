3c2db73915d4827c526ce0259623d615def9940a
Gabriel Nodari <103973562+Nodarixt4@users.noreply.github.com>
2025-03-02 08:55:11 -0400

    v5.0

--- Tentativa2/funcoes.py
+++ Tentativa2/funcoes.py
@@ -27,29 +27,43 @@ def encontrarCoordenada(matriz, numero):
                 return x,y
     return -1
 #################################################################################################
-def manhattan(matrizAtual, matrizObjetivo):
-    totalDiferenca = 0  
+def manhattanOUhaming(matrizAtual, matrizObjetivo,b):
 
-    #print("\nMatriz Atual:")
-    #print(matrizAtual)
-    #print("\nMatriz Objetivo:")
-    #print(matrizObjetivo)
+    if b==1:
+        totalDiferenca = 0  
 
-    for numeroObservado in range(1, 9):  # Ignora o 0
-        posicaoX1, posicaoY1 = encontrarCoordenada(matrizAtual, numeroObservado)
-        posicaoX2, posicaoY2 = encontrarCoordenada(matrizObjetivo, numeroObservado)
+        #print("\nMatriz Atual:")
+        #print(matrizAtual)
+        #print("\nMatriz Objetivo:")
+        #print(matrizObjetivo)
 
-        diferencaX = abs(posicaoX1 - posicaoX2)
-        diferencaY = abs(posicaoY1 - posicaoY2)
-        diferencaDeEstados = diferencaX + diferencaY
+        for numeroObservado in range(1, 9):  # Ignora o 0
+            posicaoX1, posicaoY1 = encontrarCoordenada(matrizAtual, numeroObservado)
+            posicaoX2, posicaoY2 = encontrarCoordenada(matrizObjetivo, numeroObservado)
 
-        totalDiferenca += diferencaDeEstados  
+            diferencaX = abs(posicaoX1 - posicaoX2)
+            diferencaY = abs(posicaoY1 - posicaoY2)
+            diferencaDeEstados = diferencaX + diferencaY
 
-        #print(f"Estado == {numeroObservado}")
-        #print(f"Diferença: {diferencaDeEstados}")
+            totalDiferenca += diferencaDeEstados  
 
-    #print(f"Distância Manhattan Total: {totalDiferenca}")
-    return totalDiferenca
+            #print(f"Estado == {numeroObservado}")
+            #print(f"Diferença: {diferencaDeEstados}")
+
+        #print(f"Distância Manhattan Total: {totalDiferenca}")
+        return totalDiferenca
+
+    if b==0:
+        totalDiferenca = 0  
+
+        for x in range(3):
+            for y in range(3):
+                if matrizAtual[x, y] != 0 and matrizAtual[x, y] != matrizObjetivo[x, y]:  
+                    totalDiferenca += 1  
+
+        return totalDiferenca
+    return 404
+
 #################################################################################################
 def movimentosValidos(x, y):
     movimentos = []
@@ -74,10 +88,11 @@ def moverZero(matriz, x, y, novo_x, novo_y):
     novaMatriz[x, y], novaMatriz[novo_x, novo_y] = novaMatriz[novo_x, novo_y], novaMatriz[x, y] #parte q o Vitor explicou
     return novaMatriz
 #################################################################################################
-def AestralDosDeuses(matrizInicial):
+def AestralDosDeuses(matrizInicial,seletor2):
+
     matrizObjetivo = gerarMatrizObjetivo()
     fronteira = []
-    heapq.heappush(fronteira, (manhattan(matrizInicial, matrizObjetivo), 0, tuple(map(tuple, matrizInicial)), [])) #adiciona um novo elemento à fila de prioridade frontei
+    heapq.heappush(fronteira, (manhattanOUhaming(matrizInicial, matrizObjetivo,seletor2), 0, tuple(map(tuple, matrizInicial)), [])) #adiciona um novo elemento à fila de prioridade frontei
     visitados = set() #com busca rápida e n permite duplicadas
     
     while fronteira:
@@ -95,6 +110,27 @@ def AestralDosDeuses(matrizInicial):
             if novaMatrizTuple not in visitados:
                 visitados.add(novaMatrizTuple)
                 novoCaminho = caminho + [novaMatriz]
-                heapq.heappush(fronteira, (g + 1 + manhattan(novaMatriz, matrizObjetivo), g + 1, novaMatrizTuple, novoCaminho))
+                heapq.heappush(fronteira, (g + 1 + manhattanOUhaming(novaMatriz, matrizObjetivo,seletor2), g + 1, novaMatrizTuple, novoCaminho))
     pass
+#################################################################################################
+def ehresolvido(matriz):
+    a=0
+    """Verifica se um estado do jogo 8-puzzle é resolvível."""
+    lista = matriz.flatten()
+    lista = lista[lista != 0]  # Remove o espaço vazio (0)
+    inversoes = 0
+    
+    for i in range(len(lista)):
+        for j in range(i + 1, len(lista)):
+            if lista[i] > lista[j]:
+                inversoes += 1 #vai somando as inversões de estados 
+    
+    if inversoes % 2 == 0: 
+        print("É resolvivel")
+        a=1
+    if inversoes % 2 != 0: 
+        print("Não é resolvivel")
+        a=0
+    return a
+
 ######################################## CAMPO DE TESTE #########################################
--- Tentativa2/main.py
+++ Tentativa2/main.py
@@ -2,27 +2,30 @@ import numpy as np
 import funcoes as fun
 
 ##########################################################################
-seletor = int(input("-1 Matriz inicial aleatória\n-2 Pré determinada \n-3 Escrever matriz \nResposta: "))
+seletor1 = int(input("-1 Matriz inicial aleatória\n-2 Pré determinada \n-3 Escrever matriz \nResposta: "))
+seletor2 = int(input("Resolver com 1-Manhattan ou 0-Haming? "))
 
 matrizDeterminada = np.zeros((3, 3), dtype=int)
 
-if seletor == 3:
+if seletor1 == 3:
     for linha in range(matrizDeterminada.shape[0]):  
         for coluna in range(matrizDeterminada.shape[1]):  
             matrizDeterminada[linha, coluna] = int(input(f"Posição[{linha+1}][{coluna+1}]: "))
 
-matrizInicial = fun.gerarMatrizDesorganizada(seletor, matrizDeterminada)
+matrizInicial = fun.gerarMatrizDesorganizada(seletor1, matrizDeterminada)
 print("Matriz inicial:")
 print(matrizInicial)
+a=fun.ehresolvido(matrizInicial)
 
-solucao = fun.AestralDosDeuses(matrizInicial)
-if solucao:
-    print("\nGarantido...", len(solucao), "movimentos e tá resolvido.")
-    for passo, estado in enumerate(solucao):
-        print("\nPasso", passo + 1)
-        print(estado)
-else:
-    print("\nMoio...achei foi nothing!")
+if a == 1:
+    solucao = fun.AestralDosDeuses(matrizInicial,seletor2)
+    if solucao:
+        print("\nGarantido...", len(solucao), "movimentos e tá resolvido.")
+        for passo, estado in enumerate(solucao):
+            print("\nPasso", passo + 1)
+            print(estado)
+    else:
+        print("\nMoio...achei foi nothing!")
 
 
 #Se demorar dms pra achar resultado, roda dnvkkkkkkk.  Ela ainda é muito mais "artificial" do  q "inteligente".
